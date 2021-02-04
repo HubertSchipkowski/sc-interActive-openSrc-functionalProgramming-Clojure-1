d8b4a50bbc45c1f02bf9a873f9e84ffd9b9b8af0
Hubert Schipkowski <schipkowski@sincerelycontent.com>
2021-02-04 15:10:41 +0100

    210203c

--- learnclojure/project.clj
+++ learnclojure/project.clj
@@ -1,6 +1,6 @@
 (defproject learnclojure "0.1.0-SNAPSHOT"
   :description "Exploring Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://sincerelycontent.com"
   :license {:name "(c) Hubert Schipkowski)"
             :url "https://www.sincerelycontent.com"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
@@ -12,4 +12,6 @@
 42;
 (+ 42 8)
 
+
+(- 32 192)
 
